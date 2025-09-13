c3aca0d4ab16c35de35034b8ad3680e2586f8e93
Pedro Inácio Rodrigues Pontes <pedroinaciopontes@gmail.com>
2025-09-13 16:22:33 -0300

    remocao dos logs

--- administrator/logs/error.php
+++
@@ -1,7 +0,0 @@
-#
-#<?php die('Forbidden.'); ?>
-#Date: 2025-09-12 18:36:12 UTC
-#Software: Joomla! 5.3.3 Stable [ Timu ] 19-August-2025 16:00 GMT
-
-#Fields: datetime	priority clientip	category	message
-2025-09-12T18:36:12+00:00	INFO ::1	joomlafailure	Nome de usuário e senha incorretos ou você ainda não possui uma conta.
